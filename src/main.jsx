c4b3a8e257213e27257878fa580e419e8b2b5995
erikvlcak <erikvlcak1@gmail.com>
2024-02-02 18:56:34 +0100

    Add @headlessui/react and @heroicons/react dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1 className="text-red-500 text-5xl border-t-8">Ahoj toto je React</h1>
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 )
